2ea0f2105f4eef08f654f0e199b89e538bc57874
rhonin <fatahzade@gmail.com>
2022-10-01 05:08:23 +0330

    Added createVote and giveRightToVote scripts

--- README.md
+++ README.md
@@ -1,13 +1,46 @@
-# Sample Hardhat Project
+# Weekend Project 2: Ballot Scripts
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
+## Development
 
-Try running some of the following tasks:
-
-```shell
-npx hardhat help
-npx hardhat test
-REPORT_GAS=true npx hardhat test
-npx hardhat node
-npx hardhat run scripts/deploy.ts
+```bash
+yarn install
+yarn hardhat compile
 ```
+
+Then create a file named `.env` with the following content and fill values:
+
+```env
+ALCHEMY_API_KEY=
+GOERLI_PRIVATE_KEY=
+BALLOT_ADDRESS=0x79eEcC3b5e50598129EaF64f7f86f65458E071Cd
+```
+
+## Scripts
+
+### Create Vote
+
+```bash
+yarn hardhat createVote --account 0xD9aF6C670B49C4b1239B86bb472E877f5BdF13Bf
+```
+
+Example TX: https://goerli.etherscan.io/tx/0x6baeef448ff447d6cfaf0cf1f58deb73138d11835021ff8beeb0297193386a0c
+
+### Give Right to Vote
+
+```bash
+ yarn hardhat giveRightToVote --account 0xD9aF6C670B49C4b1239B86bb472E877f5BdF13Bf
+```
+
+_Note: Only `chairman`(deployer) can call this script_
+
+Example TX: TODO
+
+## Helper Fucntions
+
+### Get Ballot Contract
+
+This function creates an instance of `Ballot` contract using `BALLOT_ADDRESS` variable in the `.env` file as address and `accounts[0]` as signer.
+
+### Get Etherscan Tx Link
+
+This function returns `Etherscan` link to Tx.
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,7 @@
 import * as dotenv from "dotenv";
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 dotenv.config();
 
@@ -14,10 +15,11 @@ const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
 const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY;
 
 const config: HardhatUserConfig = {
+  defaultNetwork: "goerli",
   paths: { tests: "tests" },
   solidity: "0.8.17",
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
   networks: {
     goerli: {
@@ -35,5 +37,59 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
+const getBallotContract = async (hre: HardhatRuntimeEnvironment) => {
+  const ballotAddres = process.env.BALLOT_ADDRESS;
+
+  if (!ballotAddres) {
+    throw "BALLOT_ADDRESS is not set in .env file";
+  }
+  if (!hre.ethers.utils.isAddress(ballotAddres)) {
+    throw "BALLOT_ADDRESS is not a valid address";
+  }
+  const accounts = await hre.ethers.getSigners();
+  const signer = accounts[0];
+
+  const ballotContractFactory = await hre.ethers.getContractFactory(
+    "Ballot",
+    signer
+  );
+  return ballotContractFactory.attach(ballotAddres);
+};
+
+const getEterscanTxLink = (txHash: String, hre: HardhatRuntimeEnvironment) => {
+  return `https://${
+    hre.network.name === "mainnet" ? "" : hre.network.name
+  }.etherscan.io/tx/${txHash}`;
+};
+
+task("createVote", "Creating Vote Object")
+  .addParam("account", "Voter's account")
+  .setAction(async (taskArgs, hre) => {
+    const ballotContract = await getBallotContract(hre);
+    const account = hre.ethers.utils.getAddress(taskArgs.account);
+    console.log("Creating a vote object:");
+    const vote = {
+      delegate: account,
+      vote: 0,
+      voted: false,
+      weight: 1,
+    };
+    const createVoteTx = await ballotContract.createVoter(account, vote);
+    console.log("Vote created!");
+    console.log("Etherscan link:", getEterscanTxLink(createVoteTx.hash, hre));
+  });
+
+task("giveRightToVote", "Giving voting rights to an account")
+  .addParam("account", "Voter's account")
+  .setAction(async (taskArgs, hre) => {
+    const ballotContract = await getBallotContract(hre);
+    const account = hre.ethers.utils.getAddress(taskArgs.account);
+    const giveRightToVoteTx = await ballotContract.giveRightToVote(account);
+    console.log("Voting right granted!");
+    console.log(
+      "Etherscan link:",
+      getEterscanTxLink(giveRightToVoteTx.hash, hre)
+    );
+  });
 
 export default config;
