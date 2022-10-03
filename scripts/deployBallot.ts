ac47816f944e6976a5110ca7d712181f0927f49c
bryanlaufly <112999106+bryanlaufly@users.noreply.github.com>
2022-10-03 09:52:51 +0800

    Update deployBallot.ts
    Refactor for loop to .map ( bytes32Array )

--- scripts/deployBallot.ts
+++ scripts/deployBallot.ts
@@ -1,15 +1,8 @@
 import { ethers } from "hardhat";
 
-function convertStringArrayToBytes32(array: string[]) {
-  const bytes32Array = [];
-  for (let index = 0; index < array.length; index++) {
-    bytes32Array.push(ethers.utils.formatBytes32String(array[index]));
-  }
-  return bytes32Array;
-}
-
 async function main() {
   const PROPOSALS = ["Proposal 1", "Proposal 2", "Proposal 3"];
+  const bytes32Array = PROPOSALS.map( prop => ethers.utils.formatBytes32String(prop))
   const [deployer] = await ethers.getSigners();
 
   console.log("Deploying contracts with the account:", deployer.address);
@@ -17,7 +10,7 @@ async function main() {
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
   const ballotFactory = await ethers.getContractFactory("Ballot");
-  const ballotContract = await ballotFactory.deploy(convertStringArrayToBytes32(PROPOSALS));
+  const ballotContract = await ballotFactory.deploy(bytes32Array);
   await ballotContract.deployed();
 
   console.log("Contract address:", ballotContract.address);
